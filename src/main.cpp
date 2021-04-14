178d47d57448332cb62e16d7320a47a51cb5e7dc
bodguy <alsrudgns99@gmail.com>
2021-04-15 00:36:19 +0900

    cmake

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,14 @@
 #include "Sphere.h"
 #include <algorithm>
 #include <vector>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 const unsigned int width = 640;
 const unsigned int height = 480;
 constexpr float aspectRatio = (float)width / (float)height;
-unsigned int frameBuffer[width * height];
+const unsigned int component = 4;
+uint8_t *frameBuffer = new uint8_t[width * height * component];
 
 int winningObjectIndex(const std::vector<float> &intersections) {
   if (intersections.empty()) {
@@ -19,29 +22,29 @@ int winningObjectIndex(const std::vector<float> &intersections) {
   if (intersections.size() == 1) {
     if (intersections[0] > 0) {
       return 0;
-    } else {
-      return -1;
     }
+    
+    return -1;
   }
 
   // find maximum value
   float max = *std::max_element(std::begin(intersections), std::end(intersections));
   // then start from the max value find min positive value
+  int indexOfMin = 0;
   if (max > 0) {
-    auto pos = std::min_element(std::begin(intersections), std::end(intersections),
-                                [](const float t1, const float t2) {
-                                  return t1 > 0 && (t2 <= 0 || t1 < t2);
-                                });
-    return std::distance(std::begin(intersections), pos);
+    for (int index = 0; index < intersections.size(); index++) {
+      if (intersections[index] > 0 && intersections[index] <= max) {
+        max = intersections[index];
+        indexOfMin = index;
+      }
+    }
+
+    return indexOfMin;
   }
 
   // all the intersections were negative
   return -1;
 }
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 void render() {
   Vec3 camPos{3, 1.5, -4};
@@ -65,10 +68,9 @@ void render() {
 
   float xAmount, yAmount;
 
+  int index = 0;
   for (int x = 0; x < width; x++) {
     for (int y = 0; y < height; y++) {
-      unsigned int pixelPosition = width * x + y;
-
       if (width > height) {
         xAmount = ((x + 0.5) / width) * aspectRatio -
                   ((width / height) / (float)height) / 2;
@@ -83,9 +85,7 @@ void render() {
       }
 
       Vec3 camRayOrigin = cam.position;
-      Vec3 camRayDirection =
-          (camDir + (camRight * (xAmount - 0.5) + (camDown * (yAmount - 0.5))))
-              .makeNormalize();
+      Vec3 camRayDirection = (camDir + (camRight * (xAmount - 0.5) + (camDown * (yAmount - 0.5)))).makeNormalize();
       Ray camRay{camRayOrigin, camRayDirection};
       std::vector<float> intersections;
 
@@ -94,22 +94,24 @@ void render() {
       }
 
       int winningIndex = winningObjectIndex(intersections);
-      printf("%d", winningIndex);
 
       if (winningIndex == -1) {
-        frameBuffer[pixelPosition] = 0x00000000;
+        frameBuffer[index++] = 0;
+        frameBuffer[index++] = 0;
+        frameBuffer[index++] = 0;
+        frameBuffer[index++] = 255;
       } else {
         Color objectColor = sceneObjects[winningIndex]->color;
-        frameBuffer[pixelPosition] = 0xff000000;
+        frameBuffer[index++] = Color::toByte(objectColor.r);
+        frameBuffer[index++] = Color::toByte(objectColor.g);
+        frameBuffer[index++] = Color::toByte(objectColor.b);
+        frameBuffer[index++] = Color::toByte(objectColor.a);
       }
     }
   }
+
+  stbi_write_png("./raytracer.png", width, height, component, frameBuffer, width * component);
+  delete[] frameBuffer;
 }
 
-#ifdef __cplusplus
-}
-#endif
-
-int main() {
-    render();
-}
+int main() { render(); }
